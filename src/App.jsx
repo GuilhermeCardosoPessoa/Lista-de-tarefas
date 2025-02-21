76a7fc9e26c8de0599fed5de1f6de811dea8bcf5
GuilhermeCardosoPessoa <guilherme.pessoa2@fatec.sp.gov.br>
2025-02-21 14:35:22 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+import TodoForm from "./components/Todoform";
+
+import Todo from "./components/Todo";
+
+import "./App.css";
+import Search from "./components/Search";
+
+function App() {
+  const[todos, setTodos] = useState([
+  ])
+
+  const addTodo = (text, category) => {
+
+    const newTodos = [
+      ...todos, 
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false
+      },
+  ]
+
+  setTodos(newTodos);
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) => 
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos)
+  }
+
+  const [search, setSearch] = useState("")
+
+
+  return (
+    <>
+      <div className="app">
+        <h1>Lista de tarefas</h1>
+        <Search search={search} setSearch={setSearch}/>
+        <div className="todo-list">
+          {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map ((todo) => (
+            <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
+          ))}
+        </div>
+        <TodoForm addTodo={addTodo}/>
+      </div>
+    </>
+  )
+}
+
+export default App
